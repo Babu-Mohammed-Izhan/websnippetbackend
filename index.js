f365ada64a94c6489d7706bf710b7119fa98c511
Izhan <babuizhan1234@gmail.com>
2021-12-04 12:20:10 +0530

    changed verceljson

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const Snippet = require("../models/snippet");
+const Snippet = require("./models/snippet");
 
 app.use(cors());
 app.use(express.json());
